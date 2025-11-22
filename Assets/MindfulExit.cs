de00855946b8a048cbdc05e6fb4e20d21f4f55bc
annachxn <chenanna@ufl.edu>
2025-11-22 05:07:13 -0500

    added mindful exit

---
+++ Assets/MindfulExit.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class ExitMindfulTrigger : MonoBehaviour
+{
+    public Animator fadeAnimator;
+    public GameObject exitPrompt;
+    private bool playerInside = false;
+
+    void Update()
+    {
+        if (playerInside && Keyboard.current.mKey.wasPressedThisFrame)
+        {
+            fadeAnimator.SetTrigger("StartFade");
+            exitPrompt.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+            exitPrompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+            exitPrompt.SetActive(false);
+        }
+    }
+}

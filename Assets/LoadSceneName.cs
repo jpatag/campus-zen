8a4cd1bd04524c7555a9b6e2c854c5a64d4afe16
annachxn <chenanna@ufl.edu>
2025-12-02 02:14:19 -0500

    updated rigs in begin and end

--- Assets/LoadSceneName.cs
+++ Assets/LoadSceneName.cs
@@ -4,12 +4,12 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     // Name of the scene to load
+    public string tagName;
     public string sceneName;
 
     private void OnTriggerEnter(Collider other)
     {
-        // Only trigger when player enters
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(tagName))
         {
             SceneManager.LoadScene(sceneName);
         }
---
+++ Assets/PortalSceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+using Unity.XR.CoreUtils;   // ← THIS is the missing namespace
+
+public class PortalSceneLoader : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Use GetComponentInParent in case the collider is on a child object
+        XROrigin rig = other.GetComponentInParent<XROrigin>();
+
+        if (rig != null)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
